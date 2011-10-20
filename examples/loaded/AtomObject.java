04fe667779038082dacfe7d02b5d1e3671f564b0
Brad Lovering <blovering@splunk.com>
2011-10-19 18:11:10 -0700

    Add sample that explores simple Atom deserialization.

---
+++ examples/loaded/AtomObject.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2011 Splunk, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"): you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+class AtomObject {
+    String Id;
+    Properties Links = new Properties();
+    String Title;
+    String Updated;
+
+    AtomObject() {}
+
+    void init(Element element) {
+        String name = element.getTagName();
+        if (name.equals("id")) {
+            this.Id = element.getTextContent().trim();
+        } 
+        else if (name.equals("link")) {
+            String rel = element.getAttribute("rel");
+            String href = element.getAttribute("href");
+            this.Links.put(rel, href);
+        }
+        else if (name.equals("title")) {
+            this.Title = element.getTextContent().trim();
+        }
+        else if (name.equals("updated")) {
+            this.Updated = element.getTextContent().trim();
+        }
+        else if (name.equals("author") || name.equals("generator")) {
+            // Ignore
+        }
+        else {
+            // UNDONE: Warning
+            System.out.format("Unrecognized element: '%s'\n", name);
+        }
+    }
+
+    // Initialize the AtomObject based on the contents of the given XML
+    // element.
+    void load(Element element) {
+        for (Node child = element.getFirstChild();
+             child != null;
+             child = child.getNextSibling()) 
+        {
+            if (child.getNodeType() != Node.ELEMENT_NODE) 
+                continue;
+            init((Element)child);
+        }
+    }
+}
---
+++ examples/loaded/Program.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2011 Splunk, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"): you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import com.splunk.Service;
+import com.splunk.http.ResponseMessage;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class Program extends com.splunk.sdk.Program {
+    public static void main(String[] args) {
+        Program program = new Program();
+        try {
+            program.init(args).run();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public Service connect() throws IOException {
+        Service service = new Service(this.host, this.port, this.scheme);
+        service.login(this.username, this.password);
+        return service;
+    }
+
+    void printAtomObject(AtomObject item) {
+        System.out.format("Id = '%s'\n", item.Id);
+        System.out.format("Title = '%s'\n", item.Title);
+        System.out.format("Updated = '%s'\n", item.Updated);
+        for (Map.Entry<String, String> entry : item.Links.entrySet()) {
+            // System.out.format("Link %s => %s\n", entry.getKey(), entry.getValue());
+            System.out.format("Link %s => ...\n", entry.getKey());
+        }
+    }
+
+    void printFeed(AtomFeed feed) {
+        printAtomObject(feed);
+        System.out.format("ItemsPerPage = %d\n", feed.ItemsPerPage);
+        System.out.format("StartIndex = %d\n", feed.StartIndex);
+        System.out.format("TotalResults = %d\n", feed.TotalResults);
+        for (AtomEntry entry : feed.Entries.values()) {
+            System.out.format("**********\n");
+            printAtomEntry(entry);
+        }
+    }
+
+    void printAtomEntry(AtomEntry entry) {
+        printAtomObject(entry);
+        System.out.format("Published = '%s'\n", entry.Published);
+        System.out.format("Content = '%s'\n", entry.Content);
+    }
+
+    public void run() throws Exception {
+        Service service = connect();
+
+        String path = this.args.length > 0 ? this.args[0] : "/";
+        ResponseMessage response = service.get(path);
+
+        int status = response.getStatus();
+        if (status != 200)
+            throw new RuntimeException(String.format("HTTP Error: %d", status));
+
+        AtomFeed feed = AtomFeed.create(response.getContent());
+
+        printFeed(feed);
+    }
+}
+
