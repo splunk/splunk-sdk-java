a2b29611e27d4e72fa5482df0e117ade3849c20e
Wim Colgate <wcolgate@splunk.com>
2011-11-09 17:44:59 -0800

    update application test

--- tests/com/splunk/ApplicationTest.java
+++ tests/com/splunk/ApplicationTest.java
@@ -38,20 +38,52 @@ public class ApplicationTest extends TestCase {
                 .login(program.username, program.password);
     }
 
+    private Service waitForSplunk() throws Exception {
+        // there is still a race condition here: if the restart takes more
+        // than 5 seconds to percolate through splunk, apps will not be
+        // reset
+        int retry = 10;
+        while (retry > 0) {
+            Thread.sleep(5000); // 5 seconds
+            retry = retry-1;
+            try {
+                return connect();
+            }
+            catch (Exception e) {
+                // server not back yet
+            }
+        }
+        Assert.fail("Splunk service did not restart");
+        return null;
+    }
+
+    private Service cleanApp(String appName, Service service) throws Exception {
+        service.restart();
+        service = waitForSplunk();
+        EntityCollection<Application> apps = service.getApplications();
+        apps.remove(appName);
+        return waitForSplunk();
+    }
+
     @Before public void setUp() {
         this.program.init(); // Pick up .splunkrc settings
     }
 
-    // Nota Bene: deleting an app, then creating one requires a splunk reboot in between.
+    // Nota Bene: Splunk needs to be restarted whenever an app is deleted
+    // (more precisely, needs to be restarted after an app is deleted, AND
+    // you want to manipulate, or create a new one in its place).
+    // This test assumes the worst case, and will restart splunk
+    // before an after an application deletion -- to correct for invalid
+    // splunk application state.
     @Test public void testApps() throws Exception {
         Service service = connect();
-
         EntityCollection<Application> apps = service.getApplications();
 
         if (apps.containsKey("sdk-tests")) {
-            apps.remove("sdk-tests");
+            service = cleanApp("sdk-tests", service);
         }
 
+        apps = service.getApplications();
         Assert.assertEquals(false, apps.containsKey("sdk-tests"));
 
         apps.create("sdk-tests");
@@ -65,7 +97,24 @@ public class ApplicationTest extends TestCase {
         app.isManageable();
         app.isVisible();
 
-        // UNDONE: (llike python tests) are we exposing 'author' in AtomObjects
+        // archive (package) the application
+        ApplicationArchive appArchive = app.archive();
+        Assert.assertTrue(appArchive.getAppName().length() > 0);
+        Assert.assertTrue(appArchive.getFilePath().length() > 0);
+        Assert.assertTrue(appArchive.getUrl().length() > 0);
+
+        ApplicationSetup appSetup = app.setup();
+        try {
+            Assert.assertTrue(appSetup.getSetupXML().length() > 0);
+        } catch (Exception e) {
+            System.out.println("EXPECTED 500 error if setup file not present");
+            System.out.println(e);
+        }
+
+        ApplicationUpdate appUpdate = app.update();
+        Assert.assertTrue(appUpdate.getContent().containsKey("eai:acl"));
+
+        // UNDONE: (like python tests) are we exposing 'author' in AtomObjects
         //Entity app = apps.get("sdk-tests");
         //Assert.assertFalse(app.getContent().get("author").equals("Splunk"));
         //Args map = new Args();
@@ -73,7 +122,8 @@ public class ApplicationTest extends TestCase {
         //app.update(map);
         //Assert.assertTrue(app.getContent().get("author").equals("Splunk"));
 
-        apps.remove("sdk-tests");
+        service = cleanApp("sdk-tests", service);
+        apps = service.getApplications();
         Assert.assertEquals(false, apps.containsKey("sdk-tests"));
     }
 }
