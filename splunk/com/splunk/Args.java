8f7b908fa81bfe3280fa0048a8727b01fbb75510
Wim Colgate <wcolgate@splunk.com>
2011-12-09 12:59:01 -0800

    Add Javadoc for Args.

--- splunk/com/splunk/Args.java
+++ splunk/com/splunk/Args.java
@@ -24,37 +24,89 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-// Value may be String or String[]
+/**
+ * The Args class extends standard HashMap.
+ *
+ * The extension is mainly for encoding arguments for UTF8 transmission
+ * to the Splunk instance in a key?value pairing for a string object, or
+ * key?value1&key?value2, etc for an array of strings object.
+ */
 public class Args extends HashMap<String, Object> {
+
+    /**
+     * Class constructor.
+     */
     public Args() { super(); }
 
+    /**
+     * Class constructor. Initialization of a single key/value pair.
+     *
+     * @param key The key name.
+     * @param value The value (String or String [])
+     */
     public Args(String key, Object value) {
         super();
         put(key, value);
     }
 
+    /**
+     * Class constructor. Initialization of a pre-existing hash map.
+     *
+     * @param values A set of key/value pairings.
+     */
     public Args(Map<String, Object> values) {
         super(values);
     }
 
+    /**
+     * Adds to an args set.
+     *
+     * @param key The key name.
+     * @param value The value. (String or String [])
+     *
+     * @return This Args set.
+     */
     public Args add(String key, Object value) {
         put(key, value);
         return this;
     }
 
+    /**
+     * Creates a new Args instance that is empty.
+     *
+     * @return The Args instance.
+     */
     public static Args create() {
         return new Args();
     }
 
+    /**
+     * Creates a new Args instance initialized with a single key/value pair.
+     *
+     * @param key The key name.
+     * @param value The value. (String or String[])
+     * @return The Args instance.
+     */
     public static Args create(String key, Object value) {
         return new Args(key, value);
     }
 
+    /**
+     * Creates a new Args instance initialized with a pre-existing hash map.
+     *
+     * @param values The pre-existing hash map.
+     * @return The Args instance.
+     */
     public static Args create(Map<String, Object> values) {
         return values == null ? new Args() : new Args(values);
     }
 
-    // Encode a single string value.
+    /**
+     * Encodes a single string with UTF8 encoding.
+     *
+     * @param value The string.
+     * @return The encoded string.
+     */
     public static String encode(String value) {
         if (value == null) return "";
         String result = null;
@@ -65,6 +117,13 @@ public class Args extends HashMap<String, Object> {
         return result;
     }
 
+    /**
+     * Encodes a hash map of String:String or String:String[] into a single UTF8
+     * encoded string.
+     *
+     * @param args The hash map.
+     * @return The string.
+     */
     public static String encode(Map<String, Object> args) {
         return Args.create(args).encode();
     }
@@ -81,6 +140,11 @@ public class Args extends HashMap<String, Object> {
         }
     }
 
+    /**
+     * Encodes an Args instance into a UTF8 encoded string.
+     *
+     * @return The UTF8 encodede string.
+     */
     public String encode() {
         StringBuilder builder = new StringBuilder();
         for (Entry<String, Object> entry : entrySet()) {
@@ -99,6 +163,16 @@ public class Args extends HashMap<String, Object> {
         return builder.toString();
     }
 
+    /**
+     * Returns the hash map value of a specific key, or the default value if
+     * the key is not found.
+     *
+     * @param args The hash map.
+     * @param key The key to look for.
+     * @param defaultValue The default value, if the key is not found.
+     * @param <T> The class type.
+     * @return The value.
+     */
     public static <T> T 
     get(Map<String, Object> args, String key, T defaultValue) {
         if (!args.containsKey(key)) return defaultValue;
