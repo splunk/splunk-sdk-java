1a34015439d8cfd1f9d39f9fcccc766657b54bb0
Wim Colgate <wcolgate@splunk.com>
2011-12-09 14:45:46 -0800

    Add javadoc for entity metadata

--- splunk/com/splunk/EntityMetadata.java
+++ splunk/com/splunk/EntityMetadata.java
@@ -16,69 +16,124 @@
 
 package com.splunk;
 
-import java.util.Map;
-
 // A class that provides access to an entities metadata. Instances of this
 // class are obtained via Entity.getMetadata.
+
+/**
+ * Representation of Entity and its subclasses meta data fields.
+ */
 public class EntityMetadata {
     private Entity entity;
 
+    /**
+     * Clas constructor.
+     *
+     * @param entity This entity.
+     */
     EntityMetadata(Entity entity) {
         this.entity = entity;
     }
 
-    // Answers if the corresponding entity's permissions can be changed.
+    /**
+     * Returns whether or not this entity's permission can be changed.
+     *
+     * @return Whether or not this entity's permission can be changed.
+     */
     public boolean canChangePermissions() {
         return getEaiAcl().getBoolean("can_change_perms", false);
     }
 
-    // Indicates that the resource can be share via an app.
+    /**
+     * Returns whether or not the resource can be shared via an app.
+     *
+     * @return Whether or not the resource can be shared via an app.
+     */
     public boolean canShareApp() {
         return getEaiAcl().getBoolean("can_share_app", false);
     }
 
-    // Indicates that the resource can be shared globally.
+    /**
+     * Returns whether or not the resource can be shared globally.
+     *
+     * @return Whether or not the resource can be shared globally.
+     */
     public boolean canShareGlobal() {
         return getEaiAcl().getBoolean("can_share_global", false);
     }
 
-    // Indicates that the resource can be shared to a specific user.
+    /**
+     * Returns whether or not the resource can be shared to a specific user.
+     *
+     * @return Whether ot nor the resource can be shared to a specific user.
+     */
     public boolean canShareUser() {
         return getEaiAcl().getBoolean("can_share_user", false);
     }
 
-    // Answers if the corresponding entity can be modified.
+    /**
+     * Returns whether or not this entity can be modified.
+     *
+     * @return Whether or not this entity can be modified.
+     */
     public boolean canWrite() {
         return getEaiAcl().getBoolean("can_write", false);
     }
 
-    // Returns the app context of this resource.
+    /**
+     * Returns the app context of this resource.
+     *
+     * @return The app context of this resource.
+     */
     public String getApp() {
         return getEaiAcl().getString("app", "system");
     }
 
+    /**
+     * Returns the record containing all the metadata information.
+     *
+     * @return The record containing all the metadata information.
+     */
     Record getEaiAcl() {
         return (Record)entity.validate().get("eai:acl");
     }
 
-    // Returns the username of the owner of this resource.
+    /**
+     * Returns the resource owner's username.
+     *
+     * @return The resource owner's username.
+     */
     public String getOwner() {
         return getEaiAcl().getString("owner");
     }
 
     // Returns the entity's permissions. The permissions are represented as
     // a Map that maps action to a list of roles that can perform the action.
+
+    /**
+     * Returns this entity's permissions. These permissions represent an
+     * allowable inclusive action:list-of-roles map.
+     *
+     * @return This entity's permissions map.
+     */
     public Record getPermissions() {
         return getEaiAcl().<Record>getValue("perms", null);
     }
 
-    // Returns a value indicating how the resources is shared, legal values
-    // are: app, global and user.
+    /**
+     * Returns how this resource is shared. Sharing values are from the list
+     * app, global and user.
+     *
+     * @return How this resource is shared.
+     */
     public String getSharing() {
         return getEaiAcl().getString("sharing");
     }
 
-    // Answers if the metadata of this entity can be changed.
+    /**
+     * Returns whether or not this entity can be modified.
+     *
+     * @return Whether or not this entity can be modified.
+     */
     public boolean isModifiable() {
         return getEaiAcl().getBoolean("modifiable", false);
     }
