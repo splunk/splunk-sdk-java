e8437dc98f5c223d5d717648c3d66289c3c2f3a3
Wim Colgate <wcolgate@splunk.com>
2011-12-12 11:53:39 -0800

    Remove UNDONE: add oneshot search that returns a responseMessage; Add javadocs.

--- splunk/com/splunk/JobCollection.java
+++ splunk/com/splunk/JobCollection.java
@@ -18,20 +18,52 @@ package com.splunk;
 
 import java.util.Map;
 
+/**
+ * Representation of a collection of jobs
+ */
 public class JobCollection extends EntityCollection<Job> {
+
+    final String oneShotNotAllowed = String.format(
+                 "Oneshot not allowed, use createOneShot method");
+    final String onlyOneShotAllowed = String.format(
+                "When using oneshot, no other exec_mode is allowed");
+    /**
+     * Class constructor.
+     *
+     * @param service The connected service instance.
+     */
     JobCollection(Service service) {
         super(service, "search/jobs", Job.class);
     }
-    
+
+    /**
+     * Creates a search job with a UTF8 pre-encoded search request. Note that
+     * a 'oneshot'request is invalid here. Please use the createOneShot method
+     * instead.
+     *
+     * @param query The search query string.
+     * @return The search job SID.
+     */
     public Job create(String query) {
+        if (query.contains("exec_mode=oneshot")) {
+            throw new RuntimeException(oneShotNotAllowed);
+        }
         return create(query, null);
     }
 
-    // UNDONE: Check for exec_mode=oneshot and throw, that request
-    // returns the results directly in the response body and can not
-    // be invoked via create - we will need to add a special oneshot(...)
-    // method that returns search results instead of a Job entity.
+    /**
+     * Creates a search job. Note that a 'oneshot' request is invalid here.
+     * Please use the craeteOneShot method instead.
+     *
+     * @param query The search query.
+     * @param args The arguments supplied to this job's creation.
+     * @return The search job SID.
+     */
     public Job create(String query, Map args) {
+        if (args.containsKey("exec_mode") &&
+            args.get("exec_mode").equals("oneshot")) {
+            throw new RuntimeException(oneShotNotAllowed);
+        }
         args = Args.create(args).add("search", query);
         ResponseMessage response = service.post(path, args);
         assert(response.getStatus() == 201);
@@ -40,11 +72,52 @@ public class JobCollection extends EntityCollection<Job> {
         return get(sid);
     }
 
+    /**
+     * Creates a 'oneshot' synchronous search with a UTF8 pre-encoded search
+     * request.
+     *
+     * @param query The search query string.
+     * @return The response message of the search.
+     */
+    public ResponseMessage createOneShot(String query) {
+       return createOneShot(query, new Args());
+    }
+
+    /**
+     * Creates a 'oneshot' synchronous search.
+     *
+     * @param query The search query.
+     * @param args The arguments to the search.
+     * @return The results of the search.
+     */
+    public ResponseMessage createOneShot(String query, Map args) {
+        if (args.containsKey("exec_mode") &&
+            !args.get("exec_mode").equals("oneshot")) {
+            throw new RuntimeException(onlyOneShotAllowed);
+        }
+        args.put("search", query);
+        if (!args.containsKey("exec_mode")) {
+            args.put("exec_mode", "oneshot");
+        }
+
+        return service.post("search/jobs/", args);
+    }
+
+    /**
+     * Returns this job's response.
+     *
+     * @return This job's response.
+     */
     @Override public ResponseMessage list() {
         return service.get(path + "?count=0");
     }
 
-    // Search jobs use the sid value as the key.
+    /**
+     * Returns this job's SID. The SID is used as this item's key.
+     *
+     * @param entry The Atom response.
+     * @return This job's SID.
+     */
     @Override protected String itemKey(AtomEntry entry) {
         return (String)entry.content.get("sid");
     }
