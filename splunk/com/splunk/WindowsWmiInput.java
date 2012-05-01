d77f86496ca0b4bba6fedef0a0617f0d1baa5c0f
Wim Colgate <wcolgate@splunk.com>
2012-05-01 16:40:54 -0700

    80 columnize javadoc comments and fix two setters.

--- splunk/com/splunk/WindowsWmiInput.java
+++ splunk/com/splunk/WindowsWmiInput.java
@@ -19,7 +19,8 @@ package com.splunk;
 import java.util.Map;
 
 /**
- * The {@code WindowsWmiInput} class represents a Windows Management Instrumentation (WMI) input.
+ * The {@code WindowsWmiInput} class represents a Windows Management
+ * Instrumentation (WMI) input.
  */
 public class WindowsWmiInput extends Input {
 
@@ -43,9 +44,11 @@ public class WindowsWmiInput extends Input {
     }
 
     /**
-     * Returns the properties (fields) collected for this class for this WMI input.
+     * Returns the properties (fields) collected for this class for this WMI
+     * input.
      *
-     * @return The list of properties collected for this class, or {@code null} if not specified.
+     * @return The list of properties collected for this class, or {@code null}
+     * if not specified.
      */
     public String [] getFields() {
         return getStringArray("fields", null);
@@ -63,14 +66,16 @@ public class WindowsWmiInput extends Input {
     /**
      * Returns a list of the WMI class instances for this WMI input.
      *
-     * @return A list of the WMI class instances, or {@code null} if not specified.
+     * @return A list of the WMI class instances, or {@code null} if not
+     * specified.
      */
     public String [] getInstances() {
         return getStringArray("instances", null);
     }
 
     /**
-     * Returns the interval at which WMI input providers are queried for this WMI input.
+     * Returns the interval at which WMI input providers are queried for this
+     * WMI input.
      *
      * @return The WMI query interval, in seconds.
      */
@@ -89,8 +94,8 @@ public class WindowsWmiInput extends Input {
     }
 
     /**
-     * Returns the main host for this WMI input. Secondary hosts are specified in
-     * the {@code server} attribute.
+     * Returns the main host for this WMI input. Secondary hosts are specified
+     * in the {@code server} attribute.
      * @see #getServer
      *
      * @return The main host.
@@ -114,9 +119,10 @@ public class WindowsWmiInput extends Input {
      * Returns a list of additional servers used in monitoring.
      * @see #getLookupHost
      *
-     * @return A comma-separated list of additional servers, or {@code null} if not specified.
+     * @return A comma-separated list of additional servers, or {@code null}
+     * if not specified.
      */
-    public String getServer() {
+    public String getServers() {
         return getString("server", null);
     }
 
@@ -130,13 +136,12 @@ public class WindowsWmiInput extends Input {
     }
 
     /**
-     * Sets the type regular expression. If specified, will only collect
-     * changes if a type name matches that regex.
+     * Sets the WMI class name.
      *
-     * @param type The type regular expression.
+     * @param classes A valid WMI class name.
      */
-    public void setClasses(String type) {
-        setCacheValue("type", type);
+    public void setClasses(String classes) {
+        setCacheValue("classes", classes);
     }
 
     /**
@@ -229,7 +234,7 @@ public class WindowsWmiInput extends Input {
      * @param servers The host from which we will monitor log events.
      */
     public void setServers(String servers) {
-        setCacheValue("servers", servers);
+        setCacheValue("server", servers);
     }
 
     /**
