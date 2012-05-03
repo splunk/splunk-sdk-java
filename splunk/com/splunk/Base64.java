45902fd40cbe3ec9e900ff15fc529dc208648375
Wim Colgate <wcolgate@splunk.com>
2012-05-03 10:06:13 -0700

    tweak base 64

--- splunk/com/splunk/Base64.java
+++ splunk/com/splunk/Base64.java
@@ -14,14 +14,14 @@
  * under the License.
  */
 
-/* adapted from wikipedia and wikihow */
-
 package com.splunk;
+
+import java.io.UnsupportedEncodingException;
 
 public class Base64 {
 
-    private static final String base64code = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            + "abcdefghijklmnopqrstuvwxyz" + "0123456789" + "+/";
+    private static final String base64code =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
     public static byte[] zeroPad(int length, byte[] bytes) {
         byte[] padded = new byte[length];
@@ -31,19 +31,19 @@ public class Base64 {
 
     public static String encode(String string) {
         String encoded = "";
-        byte[] stringArray;
+        byte[] byteArray = {};
         try {
-            stringArray = string.getBytes("UTF-8");
-        } catch (Exception ignored) {
-            stringArray = string.getBytes();
-        }
-        int paddingCount = (3 - (stringArray.length % 3)) % 3;
-        stringArray = zeroPad(stringArray.length + paddingCount, stringArray);
-        for (int i = 0; i < stringArray.length; i += 3) {
-            int j = ((stringArray[i] & 0xff) << 16) +
-                    ((stringArray[i + 1] & 0xff) << 8) +
-                    (stringArray[i + 2] & 0xff);
-            encoded = encoded + base64code.charAt((j >> 18) & 0x3f) +
+            byteArray = string.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) { assert false; }
+
+        int paddingCount = (3 - (byteArray.length % 3)) % 3;
+        byteArray = zeroPad(byteArray.length + paddingCount, byteArray);
+        for (int i = 0; i < byteArray.length; i += 3) {
+            int j = ((byteArray[i] & 0xff) << 16) +
+                    ((byteArray[i + 1] & 0xff) << 8) +
+                    (byteArray[i + 2] & 0xff);
+            encoded = encoded +
+                    base64code.charAt((j >> 18) & 0x3f) +
                     base64code.charAt((j >> 12) & 0x3f) +
                     base64code.charAt((j >> 6) & 0x3f) +
                     base64code.charAt(j & 0x3f);
