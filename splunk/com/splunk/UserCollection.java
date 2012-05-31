da5fedcbf447d0799b4ae7deebbc8d87baae7553
Andrea Pruneda <apruneda@splunk.com>
2012-05-30 17:50:51 -0700

    Doc string edits

--- splunk/com/splunk/UserCollection.java
+++ splunk/com/splunk/UserCollection.java
@@ -36,8 +36,9 @@ public class UserCollection extends EntityCollection<User> {
     /**
      * Constructs an instance of {@code UserCollection}.
      *
-     * @param service This service instance this collection is affiliated with.
-     * @param args Arguments use at instantiation, such as count and offset.
+     * @param service The connected {@code Service} instance.
+     * @param args Arguments to use when you instantiate the entity, such as 
+     * "count" and "offset".
      */
     UserCollection(Service service, Args args) {
         super(service, "authentication/users", User.class, args);
