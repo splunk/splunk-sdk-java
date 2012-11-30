2cdacb8b2e28c9243b83cc2a96f5c3ef28a5e489
Frederick Ross <fross@splunk.com>
2012-11-30 15:52:06 -0800

    Fix failure to uninstall sleep_command app at the end of a couple tests in SearchJobTest.
    Required an upgrade in the sleep_command app itself. Be sure to upgrade your version
    of the sdk-app-collection.

--- tests/com/splunk/SearchJobTest.java
+++ tests/com/splunk/SearchJobTest.java
@@ -537,7 +537,23 @@ public class SearchJobTest extends SDKTestCase {
             }
         });
 
+        final String sid = job.getSid();
         job.cancel();
+        assertEventuallyTrue(new EventuallyTrueBehavior() {
+            @Override
+            public boolean predicate() {
+                jobs.refresh();
+                return !jobs.containsKey(sid);
+            }
+        });
+        
+        // On Windows, the sleep command in our search
+        // will delay the actual end of the job process
+        // by up to 50ms. We wait for 100ms to give it a chance
+        // to die.
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {}
     }
 
     @Test
@@ -569,7 +585,23 @@ public class SearchJobTest extends SDKTestCase {
             }
         });
         
+        final String sid = job.getSid();
         job.cancel();
+        assertEventuallyTrue(new EventuallyTrueBehavior() {
+            @Override
+            public boolean predicate() {
+                jobs.refresh();
+                return !jobs.containsKey(sid);
+            }
+        });
+        
+        // On Windows, the sleep command in our search
+        // will delay the actual end of the job process
+        // by up to 50ms. We wait for 100ms to give it a chance
+        // to die.
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {}
     }
 
     @Test
