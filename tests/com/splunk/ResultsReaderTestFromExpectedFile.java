015d530a07bc1119d7424e5dd3ec8ed4545f7fea
Hannah Yun <hannahsookyun@gmail.com>
2013-03-28 12:42:52 -0700

    Changed comment to be more general

--- tests/com/splunk/ResultsReaderTestFromExpectedFile.java
+++ tests/com/splunk/ResultsReaderTestFromExpectedFile.java
@@ -130,12 +130,12 @@ public class ResultsReaderTestFromExpectedFile {
                 createXMLEventWriter(byteArrayOutputStream);
         writer.add(reader);
         writer.close();
-       
+        
         String xmlWithDtd = byteArrayOutputStream.toString();
         int lengthOfDtd = xmlWithDtd.indexOf(">") + 1;
         String dtd = xmlWithDtd.substring(0, lengthOfDtd);
         assertTrue("Expected xml to have a DTD", dtd.startsWith("<?"));
-        // remove the XML DTD <?xml version="1.0" encoding="UTF-8"?>
+        // Remove the XML DTD
         String xmlWithoutDtd = xmlWithDtd.substring(lengthOfDtd);
 
         assertEquals(xml, xmlWithoutDtd);
