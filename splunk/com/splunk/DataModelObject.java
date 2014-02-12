2735ba165c311e2db8db36504e7156407716cc6e
Fred Ross <madhadron@gmail.com>
2014-02-12 11:46:35 -0800

    Started adding calculations.

--- splunk/com/splunk/DataModelObject.java
+++ splunk/com/splunk/DataModelObject.java
@@ -1,6 +1,12 @@
 package com.splunk;
 
+import java.util.Collection;
+
 public class DataModelObject {
+    public Collection<Calculation> getCalculations() {
+        return null;
+    }
+
     public String getName() {
         return null;
     }
@@ -8,4 +14,6 @@ public class DataModelObject {
     public String[] getLineage() {
         return null;
     }
+
+
 }
---
+++ splunk/com/splunk/LookupCalculation.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2014 Splunk, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"): you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.splunk;
+
+import java.util.Collection;
+
+/**
+ * Represents a lookup calculation on a data model object.
+ *
+ * A lookup calculation takes the value of an input field and uses it to
+ * look up new values for the output fields in a lookup.
+ */
+public class LookupCalculation extends Calculation {
+    private final String lookupFieldName;
+    private final Field inputField;
+    private final String lookupName;
+
+    public LookupCalculation(DataModelObject owner, String calculationID,
+                             Collection<Field> generatedFields, String comment,
+                             String lookupName,
+                             String lookupFieldName, Field inputField) {
+        super(owner, calculationID, generatedFields, comment);
+        this.lookupName = lookupName;
+        this.lookupFieldName = lookupFieldName;
+        this.inputField = inputField;
+    }
+
+    /**
+     * @return the name of the lookup to use
+     */
+    public String getLookupName() { return this.lookupName; }
+
+    /**
+     * @return the name of the field to use in the lookup
+     */
+    public String getLookupFieldName() { return this.lookupFieldName; }
+
+    /**
+     * @return the field on the input to lookup with
+     */
+    public Field getInputField() { return this.inputField; }
+
+    /**
+     * @return name of the field on the input to lookup with
+     */
+    public String getInputFieldName() { return getInputField().getName(); }
+}
