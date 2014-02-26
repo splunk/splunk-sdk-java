9f1600935ef9cf8e56ca007326d835018c391363
Fred Ross <madhadron@gmail.com>
2014-02-26 11:22:15 -0800

    First messings around for pivot.

--- splunk/com/splunk/PivotArgs.java
+++ splunk/com/splunk/PivotArgs.java
@@ -1,7 +1,27 @@
 package com.splunk;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+
 /**
  * Created by fross on 2/13/14.
  */
 public class PivotArgs {
+    private static GsonBuilder gson = new GsonBuilder();
+
+    String dataModel = null;
+    String baseClass = null;
+
+    public PivotArgs(String dataModelName, String objectName) {
+        this.dataModel = dataModelName;
+        this.baseClass = objectName;
+    }
+
+    public PivotArgs(DataModelObject object) {
+        this.dataModel = object.getDataModel().getName();
+        this.baseClass = object.getName();
+    }
+
+
 }
