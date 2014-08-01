a787972d216ee80509ec9810ed255cddccda28a2
Sarang Metkar <smetkar@smetkar-mbp15.sv.splunk.com>
2014-08-01 14:18:13 -0700

    Sarang | Script to execute ERPMain process

---
+++ erp/scripts/erp_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Script to execute ERPMain process 
+
+if [ ! -z "$JAVA_OPTS" ]; then
+	JAVA_OPTS="$JAVA_OPTS"
+els
+	JAVA_OPTS="-Xmx512m"
+fi
+
+if [ ! -z "JAVA_HOME" ]; then
+	JAVA_CMD=$JAVA_HOME/bin/java
+fi
+
+if [ -z "$JAVA_CMD" ] || [ ! -x "$JAVA_CMD" ]; then
+	JAVA_CMD="which java"
+fi
+
+if [ -z "$JAVA_CMD" ] || [ ! -x "$JAVA_CMD" ]; then
+	echo "Unable to find java in JAVA_HOME or PATH. Please ensure JAVA_HOME is set"
+fi
+
+CLASS_NAME=$1
+CLASS_PATH=$SPLUNK_HOME/bin/jars/SplunkMR-s6.0-h2.0.jar:$bin/*
+
+$JAVA_CMD $JAVA_OPTS -cp $CLASS_PATH $CLASS_NAME
+
