a8b88266387468d1ca890f99f4468c45ade502cd
Shakeel Mohamed <smohamed@splunk.com>
2015-01-14 13:32:29 -0800

    Make Job constructor public, add example for getting a single job

---
+++ examples/com/splunk/examples/get_job/Program.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015 Splunk, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"): you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.splunk.examples.get_job;
+
+import com.splunk.*;
+
+public class Program {
+    public static void main(String[] args) {
+        Command command = Command.splunk("info").parse(args);
+        Service service = Service.connect(command.opts);
+
+        String sid = service.search("search index=_internal | head 5").getSid();
+        Job job = new Job(service, "search/jobs/" + sid);
+
+        while (!job.isDone()) {
+            job.refresh();
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        // Now job is done
+        System.out.println(job.getEventCount());
+    }
+}
--- splunk/com/splunk/Job.java
+++ splunk/com/splunk/Job.java
@@ -36,7 +36,7 @@ public class Job extends Entity {
      * @param service The connected {@code Service} instance.
      * @param path The search jobs endpoint.
      */
-    Job(Service service, String path) {
+    public Job(Service service, String path) {
         super(service, path);
     }
 
